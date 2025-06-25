1a2057ffa38d3e0d3bd29732420b4f254553b109
Abbas Rafiq <abbasrafiq82@gmail.com>
2025-06-25 15:27:56 +0500

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -16,10 +16,27 @@ const taskRoutes = require('./routes/taskRoutes');
 
 const app = express();
 
+// app.use(cors({
+//   origin: 'http://localhost:5173',
+//   credentials: true
+// }));
+const allowedOrigins = [
+  'http://localhost:5173',  // Local development
+  'https://team-task-manager-ui.onrender.com',  // Frontend on Render
+];
+
 app.use(cors({
-  origin: 'http://localhost:5173',
-  credentials: true
+  origin: (origin, callback) => {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
 }));
+
+
 app.use(express.json());
 
 app.use(session({
